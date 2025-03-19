9a1183a6759ec6e01b57b3085405c452c577dddd
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-03-19 03:23:48 +0200

    task1

--- main.pl
+++ main.pl
@@ -1,4 +1,12 @@
+# importing league_data
 :- consult(league_data).
-players_in_team(Team, Players) :-
-    findall(Player, player(Player, Team, _), Players).
+
+
+# TASK 1
+# if there is no player in the given team
+players_in_team(_, []).
+
+players_in_team(Team, [Player | Rest]) :-
+    player(Player, Team, _),
+    players_in_team(Team, Rest).
 
