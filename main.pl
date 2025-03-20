728261858afec4d108fbeeef29c8bba4929ff246
Abdelrahman Mohamed <boodymohammed26@gmail.com>
2025-03-20 23:29:12 +0200

    Task2 and not found predicate

--- main.pl
+++ main.pl
@@ -1,6 +1,10 @@
 % importing league_data
 :- consult(league_data).
 
+not_found(_, []) :- !.
+not_found(X, [X|_]) :- !,fail.
+not_found(X, [_|T]) :- not_found(X, T).
+
 % TASK 1
 
 players_in_team(Team, List) :-
@@ -33,5 +37,17 @@ myReverse([H|T],Acc,Rev):-
 myReverse(List,Rev):-
     myReverse(List,[],Rev).
 
+% Task 2
+
+team_count_by_country(Country, N):-
+    teams_by_country(Country, 0, N, []).
 
 
+teams_by_country(Country, N, Count, Teams) :-
+    team(Name, Country, _), 
+    not_found(Name, Teams),
+    N1 is N+1,
+    teams_by_country(Country, N1, Count, [Name|Teams]),
+    !.
+
+teams_by_country(_, N, N, _) :- !.
